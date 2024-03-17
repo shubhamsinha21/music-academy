5282e5572f3cecc0522ecde78b4b8821959d67bc
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-03-17 22:24:05 +0530

    TestimonialCard & Infinite Moving Card

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import FeaturedCourses from "../components/FeaturedCourses";
 import Herosection from "../components/Herosection";
+import MusicSchoolTestimonials from "../components/TestimonialCard";
 import WhyChooseUs from "../components/WhyChooseUs";
 
 export default function Home() {
@@ -8,6 +9,7 @@ export default function Home() {
       <Herosection />
       <FeaturedCourses />
       <WhyChooseUs />
+      <MusicSchoolTestimonials />
     </main>
   );
 }
