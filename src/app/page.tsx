a16b62f2202bf823818050bae47a9d9df48c9687
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-03-17 21:49:51 +0530

    WhyChooseUs & sticky scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import FeaturedCourses from "../components/FeaturedCourses";
 import Herosection from "../components/Herosection";
+import WhyChooseUs from "../components/WhyChooseUs";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Herosection />
       <FeaturedCourses />
+      <WhyChooseUs />
     </main>
   );
 }
