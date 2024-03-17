f82f1be6adb18055e85745120505b37feb9c97c6
shubhamsinha21 <84564814+shubhamsinha21@users.noreply.github.com>
2024-03-17 21:15:00 +0530

    Featured Scetion & Background Gradient

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
+import FeaturedCourses from "../components/FeaturedCourses";
 import Herosection from "../components/Herosection";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Herosection />
+      <FeaturedCourses />
     </main>
   );
 }
